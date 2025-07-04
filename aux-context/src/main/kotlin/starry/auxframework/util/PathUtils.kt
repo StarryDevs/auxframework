0d450bbdf3041d44b110a9fca03072626096f701
WowStarWorld <starworldstudio1@gmail.com>
2025-07-04 10:43:57 +0800

    fix: Paths.fromJar

--- aux-context/src/main/kotlin/starry/auxframework/util/PathUtils.kt
+++ aux-context/src/main/kotlin/starry/auxframework/util/PathUtils.kt
@@ -5,7 +5,10 @@ import java.nio.file.FileSystems
 import java.nio.file.Path
 
 object Paths {
-    fun fromJar(basePackagePath: String, jar: URI): Path = FileSystems
-        .newFileSystem(jar, mapOf<String, Any?>())
-        .getPath(basePackagePath)
+
+    fun fromJar(basePackagePath: String, jar: URI): Path =
+        runCatching { FileSystems.getFileSystem(jar) }
+            .getOrElse { FileSystems.newFileSystem(jar, mapOf<String, Any?>()) }
+            .getPath(basePackagePath)
+
 }
